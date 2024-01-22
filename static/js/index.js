c6034e388e548a11dcf62fb3955b4abfbd181c4c
Arthur Dias <arthurdias.dev@gmail.com>
2024-01-22 09:16:22 -0500

    Correct overflow-y to auto

--- static/js/index.js
+++ static/js/index.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
           return;
         }
         event.preventDefault();
-        button.click();
         return;
       }
     });
